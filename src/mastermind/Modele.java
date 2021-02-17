670be38b3a2b0d5fd2b64ac6e2724d22b667ef1c
yoshibox <meddacisohan@gmail.com>
2021-02-17 10:54:40 +0100

    Added base Modele datas

--- src/mastermind/Modele.java
+++ src/mastermind/Modele.java
@@ -1,5 +1,19 @@
 package mastermind;
 
-public class Modele {
+import java.awt.Color;
+
+public class Modele
+{
+	Color[] COULEURS;
+	int N_TENTATIVES;
+	int DIFFICULTE;
+	enum Etat {EN_COURS, GAGNE, PERDU};
 	
+	public Modele()
+	{
+		this.COULEURS = new Color[] {Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.RED, Color.ORANGE, Color.WHITE, Color.BLACK};
+		
+		this.N_TENTATIVES = 10;
+		this.DIFFICULTE = 4;
+	}
 }
